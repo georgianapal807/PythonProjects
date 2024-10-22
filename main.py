077b33afdb24f9597d3f8be872dabe5826080789
alexandrapal <georgiana.pal807@gmail.com>
2024-10-22 12:52:26 +0300

    tests

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
 user_input = input("Enter a word: ").upper()
+result = []
 
-result = [phonetic_alphabet[letter] for letter in user_input]
-print(result)
+while len(result) == 0:
+    try:
+        result = [phonetic_alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        user_input = input("Enter a word: ").upper()
+    else:
+        print(result)
 
 
